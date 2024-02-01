c551605b7042715a143ffdb2a4364fe5807c7180
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-02-01 17:10:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+//Tombola
+
+const generalNumbers = [];
+
+// Para criar uma array com numeros de 1 a 76, sem precisar inserir manualmente.
+/*const numbersofTheTombola = () => {
+    const array = [];
+    for(let i = 1; i<= 76; i++){
+    array.push(i);
+    };
+    console.log(array);//Resultado 76 dentro da Função numbersofTheTombola
+};
+    numbersofTheTombola();
+ 
+*/
+
+
+
+const createTabellone = number => {
+    const tombola = document.getElementsByClassName("tombola")[0];
+
+    for (let i = 0; i < number; i++) {
+    generalNumbers.push(i+1);
+
+    const numberCellDiv = document.createElement("div");
+    numberCellDiv.className = "numbers";
+
+    numberCellDiv.innerText = (i+1);
+
+    tombola.appendChild(numberCellDiv);
+    
+    };
+
+    console.log(generalNumbers);
+}
